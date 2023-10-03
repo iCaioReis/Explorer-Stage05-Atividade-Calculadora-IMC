91de505ec614a9c9badfaf476690ce3d28961496
iCaioReis <caioreis3186@gmail.com>
2023-10-02 23:23:39 -0300

    Finishing

--- js/script.js
+++ js/script.js
@@ -31,3 +31,6 @@ function displayResultMessage(result) {
     Modal.message.innerText = message
     Modal.open()
 }
+
+inputWeight.oninput = () => {AlertError.close()}
+inputHeight.oninput = () => {AlertError.close()}
